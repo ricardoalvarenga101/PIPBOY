50ddf0435cbecec92fa4d3c634f9077a5c573c44
= <ricardo.alvarenga@luizalabs.com>
2026-04-13 20:29:10 -0300

    feat: update retroarch-assets

--- packages/games/tools/retroarch-assets/package.mk
+++ packages/games/tools/retroarch-assets/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="9c0bd0d1015889592ad4362b800ac90ff4d019e1"
-PKG_SHA256="61d7f824dbf58403c51d4252903b44d8b7c0b346d501cdefffd04662788e767d"
+PKG_VERSION="9afd2b8a9d16fc25c5a046122bc0d2b3c965980e"
+PKG_SHA256="2d56c282372df39ef06472c5ec8f002a7515ff5b534b5fdbe619f785f639e2ef"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-assets"
 PKG_URL="https://github.com/libretro/retroarch-assets/archive/${PKG_VERSION}.tar.gz"
